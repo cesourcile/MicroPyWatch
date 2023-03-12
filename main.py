d448c93e48aba064c23d02907277fad1e9e6c246
CADET Cécile <98766071+cesourcile@users.noreply.github.com>
2023-03-12 21:53:15 +0100

    Update main.py
    Change of screens to see digital clock, analog clock and plot of BPM

--- main.py
+++ main.py
@@ -14,23 +14,27 @@ import sensor
 micropython.alloc_emergency_exception_buf(100)
 
 # On 0 : clock interface
-# On 1 : sensor interface
-# On 2 : analog clock
+# On 1 : analog clock
+# On 2 : sensor menu
 # On 3 : sensor plot
 screen = 0
 sw2 = pyb.Pin('SW2', machine.Pin.IN, machine.Pin.PULL_UP)
+
+adc = ADC(Pin('A0'))
 
 while True:
 
 	# Darkmode or not
 	ui.change_mode()
-	#print(ui.darkmode)
 
 	# sw2.value == 1 when not pressed and == 0 when pressed
 	if not sw2.value():
 		screen = (screen + 1) % 4
-		#print(screen)
 		sensor.old_bpm = -3
+		darkmode = ui.darkmode
+		color = not darkmode
+		ui.oled.fill(color)
+
 		while not sw2.value():
 			pass
 
@@ -38,18 +42,16 @@ while True:
 	if screen == 0:
 		clock.clock_interface()
 	elif screen == 1:
-		#ui.oled.fill(1)
-		#ui.draw(ui.empty_heart, 15, 21, 3)
-		#ui.oled.show()
-		sensor.sensor_2()
-	elif screen == 2:
 		clock.analog_clock_interface()
-		#ui.oled.fill(1)
-		#ui.oled.show()
+	elif screen == 2:
+		ui.oled.fill(not ui.darkmode)
+		ui.oled.text("Veuillez placer", 0, 4, ui.darkmode)
+		ui.oled.text("votre doigt sur", 0, 16, ui.darkmode)
+		ui.oled.text("le capteur afin", 0, 28, ui.darkmode)
+		ui.oled.text("de calculer", 0, 40, ui.darkmode)
+		ui.oled.text("votre BPM.", 0, 52, ui.darkmode)
+		ui.oled.show()
+		pass
 	else:
-		#ui.oled.fill(1)
-		#ui.oled.show()
-		sensor.sensor_interface()
-
-
+		sensor.sensor()
 	
